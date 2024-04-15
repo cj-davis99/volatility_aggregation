8a1a5d069de80003c5f46838e26b5c3a59657532
cj-davis99 <cole_davis@brown.edu>
2024-04-15 11:22:03 -0400

    Started temperature data generation script

---
+++ scripts/temperature_data_generation.r
@@ -0,0 +1,40 @@
+source("scripts/packages_and_seeds.r")
+
+## We want to generate our temperature for each weather station
+## Temperature will be a noisy form of the following function
+base_plt <- ggplot() +
+  xlim(0, 365) +
+  geom_function(
+    fun = function(x) -30 * cos(2 * pi * x / 365) + 50
+  ) +
+  xlab("Day") +
+  ylab("Temperature (Fahrenheit)")
+
+## Adding noise:
+noise <- rnorm(365, sd = 7.5)
+temp_data <- (-30 * cos(2 * pi * 0:364 / 365) + 50) + noise
+noise_plt <- ggplot(
+  data = data.frame(
+    x = 1:365,
+    y = temp_data
+  ),
+  aes(
+    x = 1:365,
+    y = temp_data
+  )
+) +
+  xlim(0, 365) +
+  geom_point() +
+  geom_function(
+    fun = function(x) -30 * cos(2 * pi * x / 365) + 50,
+    color = "red"
+  ) +
+  xlab("Day") +
+  ylab("Temperature (Fahrenheit)")
+
+## Import weather station location data
+ws_a <- read.csv("data/ws_data_A.csv")
+ws_b <- read.csv("data/ws_data_B.csv")
+ws_c <- read.csv("data/ws_data_C.csv")
+ws_d <- read.csv("data/ws_data_D.csv")
+
